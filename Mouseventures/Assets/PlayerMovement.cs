5fb8e34a195c000837a085e2d4961ff10cadc385
taskurotta1 <mustonenmedia@gmail.com>
2025-06-24 12:00:12 +0300

    worked with movement script

--- Mouseventures/Assets/PlayerMovement.cs
+++ Mouseventures/Assets/PlayerMovement.cs
@@ -1,41 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float horizontal;
-    public float speed = 8f;
-    public float jumpingPower = 16f;
-    private bool isFacingRight = true;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    public float speed = 5f;
+    private Vector2 movement;
+    public float jumpSpeed;
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.angularVelocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal <= 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale; 
-        }
+        float input = Input.GetAxisRaw("Horizontal");
+        movement.x = input * speed * Time.deltaTime;
+        transform.Translate(movement);
     }
 }
